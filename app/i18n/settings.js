10a495179964c6ff4e9917abf2967f15ae9e9c07
FurkanGencSt <2twofurkan@gmail.com>
2023-06-15 03:35:21 +0300

    language fixed

--- app/i18n/settings.js
+++ app/i18n/settings.js
@@ -1,5 +1,5 @@
 export const fallbackLng = "en";
-export const languages = [fallbackLng, "de", "fr", "tr", "es"];
+export const languages = [fallbackLng, "de"];
 export const defaultNS = "translation";
 
 export function getOptions(lng = fallbackLng, ns = defaultNS) {
